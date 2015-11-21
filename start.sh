c684ad50b386a87180219663d52cf632a104bb76
James <CrazyPython@users.noreply.github.com>
2015-11-21 17:00:46 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 # Install nmap
+printf "Installing Nmap..."
+rpm -vhU https://nmap.org/dist/nmap-7.00-1.x86_64.rpm
+printf " Done.\n"
+printf "Installing Zenmap..."
+rpm -vhU https://nmap.org/dist/zenmap-7.00-1.noarch.rpm
+printf " Done.\n"
+printf "Installing Ncat..."
+rpm -vhU https://nmap.org/dist/ncat-7.00-1.x86_64.rpm
+printf " Done.\n"
+printf "Installing Nping..."
+rpm -vhU https://nmap.org/dist/nping-0.7.00-1.x86_64.rpm
+printf " Done.\n"
+printf "Setup Complete."
