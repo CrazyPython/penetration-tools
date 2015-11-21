5de907c5f5af366b4047c54c3e2635dad62f8c47
James <CrazyPython@users.noreply.github.com>
2015-11-21 17:31:41 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# Install rpm
+wget http://rpm.org/releases/testing/rpm-4.13.0-rc1.tar.bz2
+bzip2 -dc rpm-4.13.0-rc1.tar.bz2 | tar xvf -
 # Install nmap
 printf "Installing Nmap..."
 rpm -vhU https://nmap.org/dist/nmap-7.00-1.x86_64.rpm
